b9d5eae9ceeb355a9a7d877f5a2d83361d5d911b
steve <chph13420146901@gmail.com>
2019-10-08 23:17:08 +0800

    [feature] Add ResourceUtil to read university list
    note: path of university_list.txt should change to relative path when running

---
+++ docs/高校列表.js
@@ -0,0 +1,444 @@
+university_list = [
+    // 985
+    {
+        'id': 'tsinghua',
+        'name': '清华大学'
+    },
+    {
+        'id': 'PKU',
+        'name': '北京大学'
+    },
+    {
+        'id': 'ruc75',
+        'name': '中国人民大学'
+    },
+    {
+        'id': 'bit1940',
+        'name': '北京理工大学'
+    },
+    {
+        'id': '5396134858',
+        'name': '北京航空航天大学'
+    },
+    {
+        'id': 'minzuuniversity',
+        'name': '中央民族大学'
+    },
+    {
+        'id': 'lovebnu',
+        'name': '北京师范大学'
+    },
+    {
+        'id': 'cau1905',
+        'name': '中国农业大学'
+    },
+    {
+        'id': 'tianjinuniversity',
+        'name': '天津大学'
+    },
+    {
+        'id': 'nankaiuni',
+        'name': '南开大学'
+    },
+    {
+        'id': 'ifudan',
+        'name': '复旦大学'
+    },
+    {
+        'id': 'chiaotunguniv',
+        'name': '上海交通大学'
+    },
+    {
+        'id': 'tongjiuniversity',
+        'name': '同济大学'
+    },
+    {
+        'id': 'ecnu',
+        'name': '华东师范大学'
+    },
+    {
+        'id': 'cqdx',
+        'name': '重庆大学'
+    },
+    {
+        'id': '3624694175',
+        'name': '四川大学'
+    },
+    {
+        'id': '3982954755',
+        'name': '电子科技大学'
+    },
+    {
+        'id': 'hunanuniversity',
+        'name': '湖南大学'
+    },
+    {
+        'id': '7127745503',
+        'name': '国防科技大学'
+    },
+    {
+        'id': 'csunews',
+        'name': '中南大学'
+    },
+    {
+        'id': 'xmunews',
+        'name': '厦门大学'
+    },
+    {
+        'id': 'ustc',
+        'name': '中国科学技术大学'
+    },
+    {
+        'id': 'nju1902',
+        'name': '南京大学'
+    },
+    {
+        'id': 'seunews',
+        'name': '东南大学'
+    },
+    {
+        'id': '1920hit',
+        'name': '哈尔滨工业大学'
+    },
+    {
+        'id': '1897zju',
+        'name': '浙江大学'
+    },
+    {
+        'id': 'xjtuofficial',
+        'name': '西安交通大学'
+    },
+    {
+        'id': 'nwsuaf',
+        'name': '西北农林科技大学'
+    },
+    {
+        'id': 'npustar',
+        'name': '西北工业大学'
+    },
+    {
+        'id': 'husters',
+        'name': '华中科技大学'
+    },
+    {
+        'id': 'wuhandaxue',
+        'name': '武汉大学'
+    },
+    {
+        'id': 'oucnews',
+        'name': '中国海洋大学'
+    },
+    {
+        'id': '1901sdu',
+        'name': '山东大学'
+    },
+    {
+        'id': 'jlunewsnet',
+        'name': '吉林大学'
+    },
+    {
+        'id': 'duters',
+        'name': '大连理工大学'
+    },
+    {
+        'id': 'neu1923',
+        'name': '东北大学'
+    },
+    {
+        'id': 'scut1117',
+        'name': '华南理工大学'
+    },
+    {
+        'id': '1892723783',
+        'name': '中山大学'
+    },
+    {
+        'id': '1878134457',
+        'name': '兰州大学'
+    },
+    // 211
+    {
+        'id': 'bjtuedu',
+        'name': '北京交通大学'
+    },
+    {
+        'id': '5053641135',
+        'name': '北京工业大学'
+    },
+    {
+        'id': 'USTB',
+        'name': '北京科技大学'
+    },
+    {
+        'id': 'buct',
+        'name': '北京化工大学'
+    },
+    {
+        'id': 'bupt',
+        'name': '北京邮电大学'
+    },
+    {
+        'id': 'BJFU',
+        'name': '北京林业大学'
+    },
+    {
+        'id': '5503968735',
+        'name': '中国传媒大学'
+    },
+    {
+        'id': 'ccomeducn',
+        'name': '中央音乐学院'
+    },
+    {
+        'id': 'UIBE',
+        'name': '对外经济贸易大学'
+    },
+    // 北京中医药大学
+    {
+        'id': 'bfsu',
+        'name': '北京外国语大学'
+    },
+    {
+        'id': 'dizhidaxue',
+        'name': '中国地质大学'
+    },
+    {
+        'id': '2100473654',
+        'name': '中国矿业大学'
+    },
+    {
+        'id': 'CUPBJ',
+        'name': '中国石油大学北京'
+    },
+    {
+        'id': 'cupl',
+        'name': '中国政法大学'
+    },
+    {
+        'id': 'CUFE',
+        'name': '中央财经大学'
+    },
+    {
+        'id': 'ncepuedu',
+        'name': '华北电力大学'
+    },
+    {
+        'id': '7097660264',
+        'name': '北京体育大学'
+    },
+    {
+        'id': 'viptmu',
+        'name': '天津医科大学'
+    },
+    {
+        'id': 'hebut19030319',
+        'name': '河北工业大学'
+    },
+    {
+        'id': 'shisu1949',
+        'name': '上海外国语大学'
+    },
+    {
+        'id': '3243026514',
+        'name': '上海大学'
+    },
+    {
+        'id': 'dhu123',
+        'name': '东华大学'
+    },
+    {
+        'id': 'SUFE',
+        'name': '上海财经大学'
+    },
+    {
+        'id': 'ECUST',
+        'name': '华东理工大学'
+    },
+    // 海军军医大学
+    {
+        'id': 'xinandaxue',
+        'name': '西南大学'
+    },
+    {
+        'id': '2241179805',
+        'name': '太原理工大学微博协会'
+    },
+    {
+        'id': 'imu1957',
+        'name': '内蒙古大学'
+    },
+    {
+        'id': 'liaoninguniversity',
+        'name': '辽宁大学'
+    },
+    {
+        'id': 'DLMU',
+        'name': '大连海事大学'
+    },
+    {
+        'id': 'dongshiwb',
+        'name': '东北师范大学'
+    },
+    {
+        'id': 'yanbiandaxue',
+        'name': '延边大学'
+    },
+    {
+        'id': 'HRBEU',
+        'name': '哈尔滨工程大学'
+    },
+    {
+        'id': '5642624713',
+        'name': '东北农业大学'
+    },
+    {
+        'id': 'nefuwb',
+        'name': '东北林业大学'
+    },
+    {
+        'id': 'sdxcb',
+        'name': '苏州大学'
+    },
+    {
+        'id': 'njnusun',
+        'name': '南京师范大学'
+    },
+    {
+        'id': 'cpu2015',
+        'name': '中国药科大学'
+    },
+    {
+        'id': 'hohai',
+        'name': '河海大学'
+    },
+    {
+        'id': 'inust',
+        'name': '南京理工大学'
+    },
+    {
+        'id': 'vjiangnan',
+        'name': '江南大学'
+    },
+    {
+        'id': 'njauxcb',
+        'name': '南京农业大学'
+    },
+    {
+        'id': 'nuaanews',
+        'name': '南京航空航天大学'
+    },
+    {
+        'id': 'ahu',
+        'name': '安徽大学'
+    },
+    {
+        'id': 'hfut1945',
+        'name': '合肥工业大学'
+    },
+    {
+        'id': 'fzuniversity',
+        'name': '福州大学'
+    },
+    {
+        'id': '3019117930',
+        'name': '南昌大学'
+    },
+    {
+        'id': 'zzugfwb',
+        'name': '郑州大学'
+    },
+    {
+        'id': 'whut2010',
+        'name': '武汉理工大学'
+    },
+    {
+        'id': 'zuel',
+        'name': '中南财经政法大学'
+    },
+    {
+        'id': 'ccnu1903',
+        'name': '华中师范大学'
+    },
+    {
+        'id': 'hzaunews',
+        'name': '华中农业大学'
+    },
+    {
+        'id': 'hunnuuni',
+        'name': '湖南师范大学'
+    },
+    {
+        'id': 'jnunews',
+        'name': '暨南大学'
+    },
+    {
+        'id': '5338220864',
+        'name': '华南师范大学'
+    },
+    {
+        'id': 'guangxidaxueguanfang',
+        'name': '广西大学'
+    },
+    {
+        'id': 'SWJTU',
+        'name': '西南交通大学'
+    },
+    {
+        'id': 'swufe1925',
+        'name': '西南财经大学'
+    },
+    {
+        'id': '3882879808',
+        'name': '四川农业大学'
+    },
+    {
+        'id': 'ynunews',
+        'name': '云南大学'
+    },
+    {
+        'id': 'gzuniversity',
+        'name': '贵州大学'
+    },
+    {
+        'id': 'northwestuni',
+        'name': '西北大学'
+    },
+    {
+        'id': 'chdeducn',
+        'name': '长安大学'
+    },
+    {
+        'id': 'snnu1944',
+        'name': '陕西师范大学'
+    },
+    {
+        'id': 'xidian',
+        'name': '西安电子科技大学'
+    },
+    // 空军军医大学
+    {
+        'id': '5974895708',
+        'name': '海南大学'
+    },
+    {
+        'id': 'nxu1958',
+        'name': '宁夏大学'
+    },
+    {
+        'id': 'qhuvip',
+        'name': '青海大学'
+    },
+    // 西藏大学
+    {
+        'id': '3835965033',
+        'name': '新疆大学'
+    },
+    {
+        'id': 'shzu',
+        'name': '石河子大学'
+    }
+];
+
+for (var i = 0; i < university_list.length; i++) {
+  console.log(university_list[i]["id"],university_list[i]["name"])
+}
---
+++ src/main/java/util/ResourceUtil.java
@@ -0,0 +1,24 @@
+package util;
+
+import java.io.*;
+import java.util.Properties;
+
+public final class ResourceUtil {
+    public static Properties getUniversityList() throws FileNotFoundException {
+        Properties props = new Properties();
+        FileInputStream in = new FileInputStream("/home/steve/Documents/专业课程文档/云计算/作业/Java-PageRank/src/main/resources/university_list.txt");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+        reader.lines().forEach(line -> {
+            String[] splits = line.split(" ");
+            props.put(splits[0], splits[1]);
+        });
+        return props;
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        Properties props = getUniversityList();
+        for (Object key : props.keySet()) {
+            System.out.println(key + "\t" + props.getProperty((String) key));
+        }
+    }
+}
